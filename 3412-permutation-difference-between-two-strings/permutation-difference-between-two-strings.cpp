afe2490293b1b5fc069296698493f50b4430dad4
Amit Dulariya <91akdulariya@gmail.com>
2026-08-24 18:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.68%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-int fun(int i,string s, string t){
-      for(int j = 0;j<t.length();j++){
-               if(s[i] == t[j]){
-                 if ((i-j)>0){
-                    return (i-j);
-                 }
-                 else return (j-i);
-               }
-        }
-        return 0;
-}
-    int findPermutationDifference(string s, string t) {
-        int  i = 0;
-        int res = 0;
-        while(i != s.length()){
-            res += fun(i,s,t);
-            i++;
-        }
 
+    int findPermutationDifference(string s, string t){
+        int pos[26] = {};
+        int res = 0;
+        for(int i = 0;i<s.length();i++){
+            pos[s[i]-'a'] = i;
+        }
+        for(int j = 0;j<t.length();j++){
+            res += abs(j-pos[t[j]-'a']);
+        }
         return res;
     }
 };
