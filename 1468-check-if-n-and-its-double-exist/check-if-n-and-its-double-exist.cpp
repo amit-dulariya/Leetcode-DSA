48e44a2316395aed55eac358735342b522c78e6a
Amit Dulariya <91akdulariya@gmail.com>
2026-09-18 22:10:40 +0530

    Time: 4 ms (12.50%) | Memory: 13.4 MB (86.07%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(arr[i]== 2*(arr[j])){
+                    return true;
+                }
+                else if(2*arr[i] == (arr[j])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
