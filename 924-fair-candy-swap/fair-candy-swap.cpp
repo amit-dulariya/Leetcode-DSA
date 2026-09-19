bfcb3e8b922bc148875cf750829018ec26567e2a
Amit Dulariya <91akdulariya@gmail.com>
2026-09-19 23:41:25 +0530

    Time: 32 ms (38.19%) | Memory: 54 MB (5.12%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.cpp
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -3,20 +3,24 @@ public:
     vector<int> fairCandySwap(vector<int>& a, vector<int>& b) {
          vector<int>ans;
          int sum1 = 0,sum2 = 0;
+         set<int>st;
          for(int i = 0;i<a.size();i++){
             sum1 += a[i];
          }
          for(int j = 0;j<b.size();j++){
             sum2 += b[j];
+            st.insert(b[j]);
          }
+         
+         int final = (sum1+sum2)/2;
          for(int i = 0;i<a.size();i++){
-            for(int j = 0;j<b.size();j++){
-                if(sum1-a[i]+b[j]==sum2+a[i]-b[j]){
-                    ans.push_back(a[i]);
-                    ans.push_back(b[j]);
-                    return ans;
-                }
+          int val = final - sum1 + a[i];
+            if(st.count(val)){
+                ans.push_back(a[i]);
+                ans.push_back(val);
+                return ans;
             }
+            
          }
          return ans;
     }
